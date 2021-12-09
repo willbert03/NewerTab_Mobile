11135ea1f4b5e20edda6f8f35a33bfd75fc5dc4f
William Leggett <williamleggett84@gmail.com>
2021-12-09 16:20:18 +0000

    Updates

--- config.js
+++ config.js
@@ -21,6 +21,16 @@ const CONFIG = {
   greetingAfternoon: 'Good afternoon,',
   greetingEvening: 'Good evening,',
   greetingNight: 'Go to Sleep!',
+
+  // Weather
+  weatherKey: '048c4d498bbd08adb540c2cd87c3d8eb',
+  weatherIcons: 'White', // 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'en', // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '52.056736',
+  defaultLongitude: '1.148220',
   
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -65,4 +75,60 @@ const CONFIG = {
       link: 'https://math.microsoft.com/en',
     },
   ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  //Icons
+  firstListIcon: 'music',
+  secondListIcon: 'book-open',
+
+  // Links
+  lists: {
+    firstList: [
+      {
+        name: 'Spotify Liked Songs',
+        link: 'spotify:user/m721lyjmo4bhqy922jubq2noe/collection',
+      },
+      {
+        name: 'Daily Dance Mix',
+        link: 'spotify:playlist/37i9dQZF1EQp9BVPsNVof1',
+      },
+      {
+        name: 'Lowkey Tech',
+        link: 'spotify:playlist/37i9dQZF1DX0r3x8OtiwEM',
+      },
+      {
+        name: 'Drum & Bass',
+        link: 'spotify:playlist/37i9dQZF1DX8jnAPF7Iiqp',
+      },
+      {
+        name: 'Dance Nation',
+        link: 'spotify:playlist/7FUhHHA0zXAPVsJdDrNxNs?si=6fba575608c24ce7'
+      }
+    ],
+    secondList: [
+      {
+        name: 'Satchel One',
+        link: 'https://www.satchelone.com/todos/upcoming',
+      },
+      {
+        name: 'OneDrive',
+        link: 'https://coplestonsuffolkschuk-my.sharepoint.com/personal/18wleggett_copleston_suffolk_sch_uk/_layouts/15/onedrive.aspx',
+      },
+      {
+        name: 'Learning Grid',
+        link: 'https://www.clickict.co.uk/mylearninggrid/mynotes.asp?usr=18wleggett&unit=10GC&key=HV%98%7Bi%A0gbn%A0',
+      },
+      {
+        name: 'Pintrest',
+        link: 'https://www.pinterest.co.uk/18wleggett/_saved/',
+      },
+      {
+        name: 'GO 4 Schools',
+        link: 'https://www.go4schools.com/parents/Login.aspx',
+      }
+    ],
+  },
 };
