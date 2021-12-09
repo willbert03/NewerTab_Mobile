4a96c272ad6f448ddfcac10144e925f7e3d6a1c8
Will <williamleggett84@gmail.com>
2021-12-09 15:46:41 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NewerTab_Mobile
+# NewerTab
  ![image](assets/img/subheader.png)
  🍱 The minimalist, elegant and hackable startpage.
  📜 Forked from https://github.com/migueravila/Bento
---
+++ config.js
@@ -0,0 +1,68 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Will',
+  imageBackground: false,
+  openInNewTab: false,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: 'Good morning!',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Go to Sleep!',
+  
+  // ┌─┐┌─┐┬─┐┌┬┐┌─┐
+  // │  ├─┤├┬┘ ││└─┐
+  // └─┘┴ ┴┴└──┴┘└─┘
+
+  // Links
+  cards: [
+    {
+      id: '1',
+      name: 'Google',
+      icon: 'search',
+      link: 'https://www.google.com/',
+    },
+    {
+      id: '2',
+      name: 'Voxi',
+      icon: 'smartphone',
+      link: 'https://www.voxi.co.uk/account',
+    },
+    {
+      id: '3',
+      name: 'Office 365',
+      icon: 'file-text',
+      link: 'https://www.office.com/',
+    },
+    {
+      id: '4',
+      name: 'Bing Visual Search',
+      icon: 'image',
+      link: 'https://www.bing.com/visualsearch',
+    },
+    {
+      id: '5',
+      name: 'Freeview TV Guide',
+      icon: 'tv',
+      link: 'https://www.freeview.co.uk/tv-guide',
+    },
+    {
+      id: '6',
+      name: 'Calculator',
+      icon: 'hash',
+      link: 'https://math.microsoft.com/en',
+    },
+  ],
+};
